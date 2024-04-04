bf5e9723945370a3cd080ff5c6c02f293fea27c1
QuentinThierry <qthierry2@gmail.com>
2024-04-04 14:11:19 -0400

    feat(parsing conf): add parsing to enter a server scope and check the openning bracket

--- main.cpp
+++ main.cpp
@@ -1,35 +1,85 @@
 #include <iostream>
 #include <fstream>
 #include <unistd.h>
+#include <vector>
+#include <exception>
+#include "Server.hpp"
 
-using namespace std;
+// returns false on eof
+static bool	wrap_getline_throw(std::fstream &s, std::string &buffer)
+{
+	getline(s, buffer);
+	if (s.fail())
+	{
+		std::cout << "Failed to read conf file" << std::endl;
+		throw std::exception(); // EXCEPTION failed to read
+	}
+	if (s.eof())
+		return false;
+	return true;
+}
 
-int main(int argc, char **argv)
+
+void	check_server_line_syntax(std::string &line, bool &has_bracket)
+{
+	int i = 0;
+	while (line[i] == ' ' || line[i] == '\t')
+		i++;
+	line.substr(i);
+	if (line.find("server") != 0)
+		throw std::exception(); // wrong conf
+
+	i = line.find_first_of('{');
+	if (i != std::string::npos)
+		has_bracket = true;
+	if (line.find_first_of('{', i + 1) != std::string::npos)
+		throw std::exception(); //wrong conf
+}
+
+
+void	enter_server(std::vector<Server> &servers, std::fstream &s, std::string &line)
+{
+	bool	has_bracket = false;
+
+	check_server_line_syntax(line, has_bracket);
+
+	std::cout << has_bracket << std::endl;
+}
+
+
+
+
+
+int	main(int argc, char **argv)
 {
 	if (argc != 2)
 		return 1;
 
-	fstream s;
+	std::fstream s;
+	std::vector<Server> servers;
 
-	s.open(argv[1], ios::in);
+	s.open(argv[1], std::ios::in);
 	if (!s.is_open())
 	{
-		cout << "Failed to open '" << argv[1] << "'" << endl;
+		std::cout << "Failed to open '" << argv[1] << "'" << std::endl;
 		return 0;
 	}
-	string buffer;
+	std::string buffer;
 	int i = 0;
 	while (true)
 	{
-		getline(s, buffer);
-		if (s.fail())
+		try
 		{
-			cout << "Failed to read '" << argv[1] << "'" << endl;
-			return 1;
+			if (!wrap_getline_throw(s, buffer))
+				break ;
+			// if server is in line
+			if (buffer.find("server") != std::string::npos)
+				enter_server(servers, s, buffer);
 		}
-		if (s.eof())
-			return 0;
-		cout << buffer << endl;
+		catch (std::exception &e)
+		{
+			std::cout << e.what() << std::endl;
+		}
 		s.clear();
 	}
 }
