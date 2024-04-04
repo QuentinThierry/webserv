6655d3848a5c93865373b7f95cf84e74eb190f57
J0hann3 <jvigny@student.42.fr>
2024-04-04 15:16:26 +0200

    feat(organization): add Makefile + shared header

---
+++ includes/shared.hpp
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   shared.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jvigny <jvigny@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/04 15:07:56 by jvigny            #+#    #+#             */
+/*   Updated: 2024/04/04 15:11:00 by jvigny           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef SHARED_HPP
+# define SHARED_HPP
+
+# define SUCCESS 0
+# define FAILURE 1
+
+# include <string>
+# include <iostream>
+# include <exception>
+
+#endif
