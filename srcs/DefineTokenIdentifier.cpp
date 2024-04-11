6a3573fe85fb4bfd54953b753d67a01baced461d
QuentinThierry <qthierry2@gmail.com>
2024-04-11 14:00:53 -0400

    feat: handle client_max_body_size

--- srcs/DefineTokenIdentifier.cpp
+++ srcs/DefineTokenIdentifier.cpp
@@ -83,12 +83,79 @@ static void _fill_error_page(std::string &token, Server &server, Location *locat
 		server.addErrorPagePath(http_error_code_to_uint16(token), "");
 }
 
+#define MAX_BITS_CLIENT_MAX_SIZE 9223372036854775807u
+#define MAX_BITS_CLIENT_MAX_SIZE_LEN 20
+#define MAX_KBITS_CLIENT_MAX_SIZE 9007199254740991u
+#define MAX_KBITS_CLIENT_MAX_SIZE_LEN 16
+#define MAX_MBITS_CLIENT_MAX_SIZE 8796093022207u
+#define MAX_MBITS_CLIENT_MAX_SIZE_LEN 13
+
+bool	_is_overflow_on_unit(std::string &token, char unit)
+{
+	uint64_t	value;
+	char *endptr;
+
+	if (unit == 'b')
+	{
+		if (token.size() > MAX_BITS_CLIENT_MAX_SIZE_LEN)
+			return true;
+		value = std::strtoul(token.c_str(), &endptr, 10);
+		if (errno == ERANGE || value > MAX_BITS_CLIENT_MAX_SIZE)
+			return true;
+	}
+	else if (unit == 'k')
+	{
+		if (token.size() > MAX_KBITS_CLIENT_MAX_SIZE_LEN)
+			return true;
+		value = std::strtoul(token.c_str(), &endptr, 10);
+		if (errno == ERANGE || value > MAX_KBITS_CLIENT_MAX_SIZE)
+			return true;
+	}
+	else if (unit == 'm')
+	{
+		if (token.size() > MAX_MBITS_CLIENT_MAX_SIZE_LEN)
+			return true;
+		value = std::strtoul(token.c_str(), &endptr, 10);
+		if (errno == ERANGE || value > MAX_MBITS_CLIENT_MAX_SIZE)
+			return true;
+	}
+	return false;
+}
+
+
+void	_parse_client_max_body_size_argument(std::string &token, Server &server)
+{
+	uint64_t	value;
+	char		unit;
+	size_t		unit_pos;
+
+	unit_pos = token.find_first_of("MmKk");
+	if (unit_pos == std::string::npos)
+		unit = 'b';
+	else
+		unit = std::tolower(token[unit_pos]);
+	if (unit_pos != std::string::npos)
+		token.erase(unit_pos, 1);
+	if (token.find_first_not_of("0123456789") != std::string::npos)
+		throw std::out_of_range("coucou");
+	if (_is_overflow_on_unit(token, unit))
+		throw std::out_of_range("coucou2");
+	value = std::atol(token.c_str());
+	if (unit == 'b')
+		server.setClientmaxBodySize(value);
+	else if (unit == 'k')
+		server.setClientmaxBodySize(value * 1024);
+	else if (unit == 'm')
+		server.setClientmaxBodySize(value * 1024 * 1024);
+}
+
+
 static void _fill_client_max_body_size(std::string &token, Server &server, Location *location, unsigned int arg_counter)
 {
 	(void)arg_counter;
-	if (location)
+	if (location || arg_counter != 1)
 		throw std::exception();
-	// _parse_client_max_body_size_argument(token, server);
+	_parse_client_max_body_size_argument(token, server);
 }
 
 // location possible
