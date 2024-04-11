96c7d9b5c7c3eaa5c9acd9a36cdaf8581fa4a26d
QuentinThierry <qthierry2@gmail.com>
2024-04-11 11:24:28 -0400

    feat: add html code error support translation into uint_16

---
+++ srcs/HtmlErrorCodes.cpp
@@ -0,0 +1,28 @@
+#include "ConfParser.hpp"
+
+bool	does_http_error_code_exist(std::string code)
+{
+	std::string const http_error_codes[] =
+	{
+		"100", "101", "102", "103",
+		"200", "201", "202", "203", "204", "205", "206", "207", "208", "226",
+		"300", "301", "302", "303", "304", "305", "306", "307", "308",
+		"400", "401", "402", "403", "404", "405", "406", "407", "408", "409",
+		"410", "411", "412", "413", "414", "415", "416", "417", "418",
+		"421", "422", "423", "424", "425", "426", "428", "429", "431", "451",
+		"500", "501", "502", "503", "504", "505", "506", "507", "508", "510", "511",
+		""
+	};
+	for (unsigned int i = 0; !http_error_codes[i].empty(); i++) {
+		if (http_error_codes[i] == code)
+			return true;
+	}
+	return false;
+}
+
+uint16_t	http_error_code_to_uint16(std::string code)
+{
+	if (!does_http_error_code_exist(code))
+		throw std::exception();
+	return (code[0] - '0') * 100 + (code[1] - '0') * 10 + (code[2] - '0');
+}
