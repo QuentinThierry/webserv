f24a9d46ee0ab1c4b7619502a44fe65096e0d712
Alex Cardona <42.alex.cardona@gmail.com>
2024-04-12 16:44:05 +0200

    [refactor:  converting success status as an enum]

--- includes/Shared.hpp
+++ includes/Shared.hpp
@@ -1,23 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   shared.hpp                                         :+:      :+:    :+:   */
+/*   Shared.hpp                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: acardona <acardona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/04 15:07:56 by jvigny            #+#    #+#             */
-/*   Updated: 2024/04/04 17:39:08 by acardona         ###   ########.fr       */
+/*   Updated: 2024/04/11 20:16:20 by acardona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef SHARED_HPP
 # define SHARED_HPP
 
-# define SUCCESS 0
-# define FAILURE 1
+typedef enum e_status
+{
+	SUCCESS,
+	FAILURE
+} e_status;
 
 # include <string>
 # include <iostream>
 # include <exception>
+# include <unistd.h>
+
+extern int	g_err_log_fd;
+
+int protected_write(int const fd, std::string const str);
 
 #endif
