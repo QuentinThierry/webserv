f4412b85f9927f2fa5a6994c584c7f3b715db57d
Alex Cardona <42.alex.cardona@gmail.com>
2024-05-05 19:20:22 +0200

    feat: add HttpRequestGet class (only parsing)

--- includes/HttpRequestGet.class.hpp
+++ includes/HttpRequestGet.class.hpp
@@ -6,7 +6,7 @@
 /*   By: acardona <acardona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/12 19:23:29 by acardona          #+#    #+#             */
-/*   Updated: 2024/04/12 19:28:51 by acardona         ###   ########.fr       */
+/*   Updated: 2024/05/05 18:52:49 by acardona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,17 +16,24 @@
 # include "HttpTools.hpp"
 # include "HttpRequest.class.hpp"
 
+# define MSG_ERR_HTTPGET_SSTREAM_FAIL "ERROR: internal: stringstream failure in the get request construction"
+# define MSG_ERR_HTTPGET_WRONG_METHOD "ERROR: internal: call to the wrong HTTP method constructor"
+
+class HttpResponse;
+
 class HttpRequestGet : public HttpRequest
 {
 	public:
 		HttpRequestGet (std::string const & str_request) throw (ExceptionHttpStatusCode);
 		HttpRequestGet ( HttpRequestGet const & model);
 		HttpRequestGet & operator= (HttpRequestGet const & model);
+		~HttpRequestGet( void );
+
+		void			process_header( void );
+		HttpResponse	generate_response( void );
 
 	private:
 		HttpRequestGet( void );
-		~HttpRequestGet( void );
-
 };
 
 # endif
---
+++ srcs/HttpRequestGet.cpp
@@ -0,0 +1,75 @@
+#include "../includes/HttpRequestGet.class.hpp"
+#include "../includes/HttpResponse.class.hpp"
+
+HttpRequestGet::HttpRequestGet (std::string const & str_request)
+	throw (ExceptionHttpStatusCode)
+	: HttpRequest()
+{
+	std::stringstream	stream_request (str_request);
+
+	if (stream_request.bad())
+		throw_http_err_with_log(HTTP_500, MSG_ERR_HTTPGET_SSTREAM_FAIL);
+	HttpRequest::init(stream_request);
+	if (*getMethod() != "GET")
+		throw_http_err_with_log(HTTP_500, MSG_ERR_HTTPGET_WRONG_METHOD);
+	//potential body unused then
+}
+
+HttpRequestGet::HttpRequestGet ( HttpRequestGet const & model)
+	: HttpRequest(model)
+{
+}
+
+HttpRequestGet & HttpRequestGet::operator= (HttpRequestGet const & model)
+{
+	if (&model != this)
+		HttpRequest::operator=(model);
+	return (*this);
+}
+
+HttpRequestGet::HttpRequestGet( void ) //unused
+{
+}
+
+HttpRequestGet::~HttpRequestGet( void )
+{
+}
+
+void			HttpRequestGet::process_header( void )
+{
+	//TODO
+}
+
+HttpResponse	HttpRequestGet::generate_response( void )
+{
+	//TODO
+	return (HttpResponse());
+}
+
+
+#if 0
+
+int g_err_log_fd = STDERR_FILENO;
+
+int main ()
+{
+	std::string request = "GET toto HTTP/1.1\nfield1:value1\n\nthis is the body\nit shouldn't appear";
+
+	try
+	{
+		HttpRequestGet get_request (request);
+
+		get_request.display_request();
+	}
+	catch (ExceptionHttpStatusCode & e)
+	{
+		e.display_error();
+	}
+	catch (std::exception & e)
+	{
+		std::cout << "Non http error: " << e.what() << std::endl;
+	}
+	return (true);
+}
+
+#endif
