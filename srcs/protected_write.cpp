e353579a5cf319b15bbe59ab3c89a181aa0fe298
Alex Cardona <42.alex.cardona@gmail.com>
2024-05-06 19:18:21 +0200

    refactor: protected write modified to potentially not print

--- srcs/protected_write.cpp
+++ srcs/protected_write.cpp
@@ -6,7 +6,7 @@
 /*   By: acardona <acardona@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/11 20:15:41 by acardona          #+#    #+#             */
-/*   Updated: 2024/04/11 22:27:14 by acardona         ###   ########.fr       */
+/*   Updated: 2024/05/06 19:14:14 by acardona         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,12 @@
 
 int protected_write(int const fd, std::string const str)
 {
-	int	n;
-	
-	n = write(fd, str.c_str(), str.size());
-	write(fd, "\n", 1);
+	int	n = 0;
+
+	if (fd >= 0)
+	{
+		n = write(fd, str.c_str(), str.size());
+		write(fd, "\n", 1);
+	}
 	return (n);
 }
